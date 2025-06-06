10aa668e3b9eb34991ecdeb74c98a2eb2fab9653
Annie <annenark@gmail.com>
2025-06-06 13:55:42 -0500

    Left off right before adding to-dos

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
     @State private var showNewTask = false
 
+    @Query var toDos: [ToDoItem]
+
+    
     var body: some View {
         VStack {
             HStack {
@@ -31,6 +35,11 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+            List {
+                    ForEach(toDos) { toDoItem in
+                        Text(toDoItem.title)
+                    }
+            }
             
         }
         if showNewTask {
